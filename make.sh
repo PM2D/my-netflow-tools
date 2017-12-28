3a8a69008ca9d6233b8aa3072898b67bf36fce5a
PM2D <m2d@sibnet.ru>
2017-12-28 17:54:57 +0700

    Compile with large file (>2Gb) support

--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Building flowtosql..."
-gcc -o flowtosql flowtosql.c cfg.c -I`pg_config --includedir` `pkg-config --cflags glib-2.0` `pkg-config --libs glib-2.0` -lpq -liniparser -std=gnu99 -O2 -pipe -fgnu89-inline -Wno-unused-result -march=native -Wall
+gcc -o flowtosql flowtosql.c cfg.c -I`pg_config --includedir` `pkg-config --cflags glib-2.0` `pkg-config --libs glib-2.0` -lpq -liniparser -std=gnu99 -O2 -pipe -fgnu89-inline -Wno-unused-result -march=native -Wall -D_LARGEFILE_SOURCE=1 -D_FILE_OFFSET_BITS=64
 echo "Building tobin..."
-gcc -o tobin tobin.c -std=gnu99 -O2 -pipe -march=native
+gcc -o tobin tobin.c -std=gnu99 -O2 -pipe -march=native -D_LARGEFILE_SOURCE=1 -D_FILE_OFFSET_BITS=64
 echo "Building frombin..."
-gcc -o frombin frombin.c -std=gnu99 -O2 -pipe -march=native
+gcc -o frombin frombin.c -std=gnu99 -O2 -pipe -march=native -D_LARGEFILE_SOURCE=1 -D_FILE_OFFSET_BITS=64
 echo "Done."
